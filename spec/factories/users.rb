e0433912f3c050ac28cdaeab55beb68b70b32317
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-10-20 12:56:49 +0900

    rubocop

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.name}
-    email                 {Faker::Internet.free_email}
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password              {password}
-    password_confirmation {password}
-    last_name             {Faker::Name.name}
-    first_name            {Faker::Name.name}
-    last_name_kana        {"マコ"}
-    first_name_kana       {"ユキナリ"}
-    date_of_birth         {"1991/01/01"}
+    password              { password }
+    password_confirmation { password }
+    last_name             { Faker::Name.name }
+    first_name            { Faker::Name.name }
+    last_name_kana        { 'マコ' }
+    first_name_kana       { 'ユキナリ' }
+    date_of_birth         { '1991/01/01' }
   end
 end
