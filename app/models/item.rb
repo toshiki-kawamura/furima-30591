721c6e0e696f14293e35af564583d8e288f589f4
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-10-20 17:57:45 +0900

    itemモデルのバリエーションとアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_many   :comments
+  has_one    :purchase
+
+  with_options presence: true do
+    
+    validates :name
+    validates :description
+    validates :category_id
+    validates :product_condition_id
+    validates :shipping_charges_id
+    validates :shipping_area_id
+    validates :days_to_ship_id
+    validates :price
+  end
+
+
 end
--- db/migrate/20201020081719_create_items.rb
+++ db/migrate/20201020081719_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name
+      t.text :description
+      t.interger :category_id
+      t.interger :product_condition_id
+      t.interger :shipping_charges_id
+      t.interger :shipping_area_id
+      t.interger :days_to_ship_id
+      t.interger :price
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
