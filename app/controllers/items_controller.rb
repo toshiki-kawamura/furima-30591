834fe021e9874ad94cca8f74a0d3b487d1ded716
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-10-21 15:36:29 +0900

    商品情報のActiveHash::Baseを各項目作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,except: [:index, :show]
+  
   def index
+  end
+
+  def new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,13 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre, :condition, :days_to_ship, :shipping_area, :shipping_charge
+
   belongs_to :user
   has_many   :comments
   has_one    :purchase
   has_one_attached :image
-  
+
   with_options presence: true do
     validates :name
     validates :description
@@ -13,6 +17,7 @@ class Item < ApplicationRecord
     validates :shipping_area_id
     validates :days_to_ship_id
     validates :price
+    validates :genre_id, numericality: { other_than: 1 } 
   end
 
 
