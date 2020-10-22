67ccf223274ac732a8a33f4badc01d0838e97a05
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-10-22 11:05:46 +0900

    itemモデルのファクトリーボットの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name                   { Faker::Name.name }
+    description            { Faker::Name.name }
+    category_id            { 2 }
+    product_condition_id   { 2 }
+    shipping_charges_id    { 2 }
+    shipping_area_id       { 2 }
+    days_to_ship_id        { 2 }
+    price                  { 500 }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe'商品出品機能' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+  it '商品画像を1枚つけることが必須であること' do
+    @item.image = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("User must exist")
+  end
+  it '商品名が必須であること' do
+  
+  end
+ 
+  it '商品の説明が必須であること' do
+  
+  end
+  it 'カテゴリーの情報が必須であること' do
+    
+  end
+  it '商品の状態についての情報が必須であること' do
+    
+  end
+  it '配送料の負担についての情報が必須であること' do
+    
+  end
+  it '発送元の地域についての情報が必須であること' do
+    
+  end
+  
+  it '発送までの日数についての情報が必須であること' do
+    
+  end
+
+  it '価格についての情報が必須であること' do
+    
+  end
+
+  it '価格の範囲が、¥300~¥9,999,999の間であること' do
+    
+  end
+
+  it '販売価格は半角数字のみ保存可能であること' do
+    
+  end
+
+  it '入力された販売価格によって、販売手数料や販売利益の表示が変わること' do
+    
+  end
+
+
+ end
+
 end
