66ab052cd440e675e2301b6af00b41b381d9a081
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-10-23 14:35:37 +0900

    ビューファイルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :comments
+  has_many :items
+  has_many :purchases
 
   KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
 
