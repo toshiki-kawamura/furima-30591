165dd94a251371c8304e112a70fd43772cccb6b8
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-10-26 19:51:53 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if user_signed_in? && current_user.id == @item.user.id 
       @item.destroy
       redirect_to root_path
     end
