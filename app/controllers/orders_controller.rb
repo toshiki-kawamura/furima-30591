3fc53157ebcf4b8f569ad1be72db3f92231f0a59
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-10-27 19:37:32 +0900

    インスタンス変数の宣言

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,13 @@
 class OrdersController < ApplicationController
   def index
-    # @order = Order.find(order_params)
+    @item = Item.find(params[:item_id])
   end
 
   def create
   end
-private
-  def order_params
+
+  private
+  # def order_params
     # params.require(:oreder).permit(:postal_code, :prefecture_id, :municipality, :address, :phone_number, :building_name).merge(user_id: current_user.id, item_id: params[:item_id])
-  end
+  # end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,6 @@
 class Order < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-
   belongs_to :purchase
 
 
