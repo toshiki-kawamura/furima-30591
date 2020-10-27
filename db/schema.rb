bb45ef6837168951462aea8b534c98ba0a52e307
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-10-27 16:54:10 +0900

    エラーの修正とマイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_093334) do
+ActiveRecord::Schema.define(version: 2020_10_27_073002) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,19 @@ ActiveRecord::Schema.define(version: 2020_10_20_093334) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code"
+    t.integer "prefecture_id"
+    t.string "municipality"
+    t.string "address"
+    t.string "building_name"
+    t.string "phone_number"
+    t.bigint "purchase_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["purchase_id"], name: "index_orders_on_purchase_id"
+  end
+
   create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "item_id"
@@ -75,6 +88,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_093334) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "purchases"
   add_foreign_key "purchases", "items"
   add_foreign_key "purchases", "users"
 end
