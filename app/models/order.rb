7b89868bba2d6b18a783b9249b93a194479461ea
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-10-27 16:51:36 +0900

    バリデーション、アソシエーションの記述

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,16 @@
 class Order < ApplicationRecord
+
+  belongs_to_active_hash :prefecture
+
+  belongs_to :purchase
+
+
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :municipality
+    validates :address
+    validates :phone_number
+  end
+  
 end
