27ca616dd74def63ec0f6ce9cff85b577a1814a9
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-10-27 22:28:24 +0900

    クレジット機能、トークンの送信

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,10 @@
 window.addEventListener('load', () => {
-  console.log("OK");
-
-  const priceInput = document.getElementById("item-price");
+  const priceInput = document.getElementById("item-price")|| document.createElement('input');
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
      const addTaxDom = document.getElementById("add-tax-price");
      addTaxDom.innerHTML = Math.floor( inputValue / 10) ;
      const profitOnSale = document.getElementById("profit");
      profitOnSale.innerHTML = inputValue - addTaxDom.innerHTML;
-    
   });
 })
