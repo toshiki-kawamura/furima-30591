e7fcfe8e48b2b61671fa6f98ed240e104c4c1003
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-10-29 09:23:34 +0900

    クレジット機能実装途中

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,12 @@ class OrdersController < ApplicationController
     binding.pry
     @order = Order.new(order_params)
     if @order.valid?
+      Payjp.api_key = "sk_test_d6a3edafe5e049a803265a86"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: order_params[:price],  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
       @order.save
       return redirect_to root_path
     else
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,8 @@
 class Purchase < ApplicationRecord
+  include ActiveModel::Model
+  attr_accessor :token
+
+
   belongs_to :user
   belongs_to :item
   has_one    :order
