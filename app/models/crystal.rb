6366e2e92bccdf9417cf2922290a9e8cc79f6a4a
toshiki-kawamura <71739349+toshiki-kawamura@users.noreply.github.com>
2020-11-02 20:16:34 +0900

    修正５

--- app/models/crystal.rb
+++ app/models/crystal.rb
@@ -13,6 +13,6 @@ class Crystal
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Order.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, building_name: building_name, address: address, phone_number: phone_number, purchase_id: purchase_id)
+    Order.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, building_name: building_name, address: address, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
